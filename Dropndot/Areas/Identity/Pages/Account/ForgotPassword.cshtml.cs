8363e501e824d3e1b1c45e1efd030e879ff753a8
goalmshahriar2019 <golamshahriar@primetechbd.com>
2019-10-27 20:15:19 +0600

    ForgotPassword Policy

--- Dropndot/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ Dropndot/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -5,7 +5,6 @@ using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Identity.UI.Services;
 using Dropndot.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -34,7 +33,6 @@ namespace Dropndot.Areas.Identity.Pages.Account
             [EmailAddress]
             public string Email { get; set; }
         }
-
         public async Task<IActionResult> OnPostAsync()
         {
             if (ModelState.IsValid)
@@ -43,24 +41,10 @@ namespace Dropndot.Areas.Identity.Pages.Account
                 if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
                 {
                     // Don't reveal that the user does not exist or is not confirmed
-                    return RedirectToPage("./ForgotPasswordConfirmation");
+                    return RedirectToPage("./RecoverAccount", new { userEmail = user.Email });
                 }
 
-                // For more information on how to enable account confirmation and password reset please 
-                // visit https://go.microsoft.com/fwlink/?LinkID=532713
-                var code = await _userManager.GeneratePasswordResetTokenAsync(user);
-                var callbackUrl = Url.Page(
-                    "/Account/ResetPassword",
-                    pageHandler: null,
-                    values: new { code },
-                    protocol: Request.Scheme);
-
-                await _emailSender.SendEmailAsync(
-                    Input.Email,
-                    "Reset Password",
-                    $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                return RedirectToPage("./ForgotPasswordConfirmation");
+                return RedirectToPage("./RecoverAccount", new { userEmail = user.Email });
             }
 
             return Page();
